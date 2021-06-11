5b24540e1cf3c1cffefccf2d8846900abad664c7
Samrat Basu <47379788+samratb001@users.noreply.github.com>
2021-06-11 14:55:13 +0530

    port commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose=require("mongoose");
 const shopRoute=require('./Routes/shopRoutes/shop');
 const customerRoute= require('./Routes/customerRoutes/customer');
-var port = 4200;
+var port = process.env.PORT || 5000;
 app.use(express.json());
 app.listen(port,()=>{console.log("Node Server is running at port " + port);});
 // mongoose.connection("mongodb+srv:PrognaSU:<password>@cluster0.bzjvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").then(()=>{
